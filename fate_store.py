069831c6a5b628cd912cddaa1493c95e82810322
Sif Oberon <muell758@d.umn.edu>
2022-10-18 18:55:06 -0500

    Set up structure of main, set up menu outline, and define general functions for app with notes. Clean up fate_store & clarify functions

--- fate_store.py
+++ fate_store.py
@@ -1,5 +1,4 @@
 """ initial set up for Mystic Fate DB, modeled after bookstore from project 2 """
-
 import sqlite3
 import os
 
@@ -7,102 +6,56 @@ db = os.path.join('database', 'fates.db')
 
 class Fate:
     """ Represents one user 'fate'.
-    This contains: (tarot card, animal, horoscope)
+    This contains: (zodiac sign, tarot card, animal, horoscope)
     """
 
-    def __init__(self, tarot_card, animal, horoscope, id):
+    def __init__(self, zodiac_sign, tarot_card, animal, horoscope):
+        self.zodiac_sign = zodiac_sign
         self.tarot_card = tarot_card
         self.animal = animal
         self.horoscope = horoscope
-        self.id = id
 
         self.fate_store = FateStore()
 
 
     def save(self):
         """ Saves a generated user fate in the program. """
-        self.fate_store._add_fate(self)
+        self.fate_store._save_fate(self)
 
-    # def delete(self):
-    #     self.fate_store._delete_fate(self)
+    def view_all_fates(self):
+        """ View all the saved Fates in the program. """
+        self.fate_store._get_all_fates(self)
 
-    
+
 class FateStore:
 
-    instance = None  # what does this do?
-
-    class __FateStore:
-
-        def __init__(self):
-            create_table_sql = 'CREATE TABLE IF NOT EXISTS fates (tarot TEXT, animal TEXT, horoscope TEXT'
-
-            conn = sqlite3.connect(db)
-
-            with conn:
-                conn.execute(create_table_sql)
-
-            conn.close()
-
-        
-        # def _add_fate(self, fate):
-        #     """Adds fate to fate_store (DB)"""
-
-        #     insert_sql = 'INSERT INTO fates (tarot, animal, horoscope) VALUES (?, ?, ?)'
-
-        #     try:
-        #         with sqlite3.connect(db) as conn:
-        #             res = conn.execute(insert_sql, (fate.tarot, fate.animal, fate.horoscope) )
-        #             new_id = res.lastrowid # figure out a new id based on what the last row in table is
-        #             fate.id = new_id #set this fate's id
-
-        #             exception_error = False # TODO set to False (if no exception, fate successfully added)
-        #             return exception_error
-
-        #     except sqlite3.IntegrityError as e:
-        #         exception_error = True # set to True, code reaches the exception
-        #         raise FateError(f'Error - this fate cannot be added.' {fate}) from e
-
-            # finally:
-            #     conn.close()
-            #     return exception_error
-
-        
-        def get_all_fates(self):
-            """ Returns all fate objects stored in DB """
-            get_all_fates_sql = 'SELECT rowid, * FROM fates'
-
-            
-            conn = sqlite3.connect(db)
-            conn.row_factory = sqlite3.Row
-            rows = conn.execute(get_all_fates_sql)
-            fates = []
-
-            for r in rows:
-                fate = Fate(r['title'], r['author'], r['read'], r['rowid'])
-                fates.append(fate)
-
-            conn.close()            
-            
-            return fates
+    def __init__(self):
+        create_table_sql = 'CREATE TABLE IF NOT EXISTS fates (zodiac_sign text, tarot_card TEXT, animal TEXT, horoscope TEXT'
+        conn = sqlite3.connect(db)
+        with conn:
+            conn.execute(create_table_sql)
+        conn.close()
 
 
-    def __new__(cls):
-        """ The __new__ magic method handles object creation. (Compare to __init__ which initializes an object.) 
-        If there's already a Bookstore instance, return that. If not, then create a new one
-        This way, there can only ever be one __Bookstore, which uses the same database. """
-        if not FateStore.instance:
-            FateStore.instance = FateStore.__FateStore()
-        return FateStore.instance
+    def _save_fate(self, fate):
+        """Adds fate to fate_store (DB)"""
+        insert_sql = 'INSERT INTO fates (zodiac_sign, tarot_card, animal, horoscope) VALUES (?, ?, ?, ?)'
+        with sqlite3.connect(db) as conn:
+            conn.execute(insert_sql, (fate.zodiac_sign, fate.tarot_card, fate.animal, fate.horoscope) )
+        conn.close()
 
 
-    def __getattr__(self, name):
-        return getattr(self.instance, name)
+    def _get_all_fates(self):
+        """ Returns all fate objects stored in DB """
+        get_all_fates_sql = 'SELECT rowid, * FROM fates'
+        conn = sqlite3.connect(db)
+        conn.row_factory = sqlite3.Row
+        rows = conn.execute(get_all_fates_sql)
+        fates = []
 
+        for r in rows:
+            fate = Fate(r['zodiac_sign'], r['tarot_card'], r['animal'], r['horoscope'])
+            fates.append(fate)
+        conn.close()
 
-    def __setattr__(self, name, value):
-        return setattr(self.instance, name, value)
-
-
-    class FateError(Exception):
-        """ For BookStore errors. """
-        pass
+        return fates
