6c27b19eb8b55e9961707f942021c863188996b6
Jordan bundy <jbundy611@gmail.com>
2022-10-20 18:12:08 -0500

    added requested spacing

--- animal_api.py
+++ animal_api.py
@@ -6,11 +6,13 @@ response = requests.get(animal_url)
 
 animal_data = response.json()
 
+
 def animal_picture_request():
     #grabs the animal picture link from the API
     animal_picture = animal_data['image_link']
 
     return animal_picture
+
 
 def animal_name_request():
     #grabs the animals name from the API
