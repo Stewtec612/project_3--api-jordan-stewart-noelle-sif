651e950e828c0d934dd7e4b1151de8d06c15d423
Stewart Christie <stewart.chrst99@gmail.com>
2022-10-21 08:48:26 -0500

    created module that will collect the needed information

--- get_user_input.py
+++ get_user_input.py
@@ -1,5 +1,7 @@
 
-
+'''
+Module that collects needed information that the other modules will use and create a faster process for asking questions
+'''
 def ask_questions():
     get_name = input('Please enter your name: ')
     get_zodiac_sign = input('What is your zodiac Sign?: ')
