2fb80adabf122604a39c22b487eb6ec1b5f67755
Noelle sdfds <you@gmail.com>
2022-10-30 20:53:59 -0500

    importing modules from the api's

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 from fate_store import _get_all_fates
 
-from animal_api import animal_picture_request
+from animal_api import animal_name_request, animal_picture_request
 from horoscope_api import horoscope_data
 from tarot_api import tarot_card
 # import API manager module 
@@ -14,7 +14,7 @@ def homepage():
 
 @app.route('/mystic_fetcher')
 def mystic_results():
-    zodiac = request.args 
+    zodiac = request.args.get('fates') 
     print(zodiac)
 
     # what was the user's birthday?
@@ -23,9 +23,10 @@ def mystic_results():
     # talk to apis
     # for example, api returns your spirit animal is zebra your horoscope is you will have a nice day 
     # this is example data, replace with data from the API 
-    animal = animal_url.get()  
-    horoscope = tarot_card.get()
-    animal_url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Plains_Zebra_Equus_quagga.jpg/1024px-Plains_Zebra_Equus_quagga.jpg'
+    animal_name = animal_name_request
+    animal_pic = animal_picture_request
+    horoscope = horoscope_data.get(horoscope)
+    tarot = tarot_card.get('tarot_data')
 
     # create new web page with results 
     return render_template('results.html', animal=animal, horoscope=horoscope, animal_image_url=animal_url)
