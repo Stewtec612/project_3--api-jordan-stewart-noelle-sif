3ddf25e1a59ddcd21bda38d26671ec9101f6c3a6
Noelle sdfds <you@gmail.com>
2022-10-30 20:12:01 -0500

    updates to app.py, trying to connect to api_managers

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, request
+from fate_store import _get_all_fates
 
+from animal_api import animal_picture_request
+from horoscope_api import horoscope_data
+from tarot_api import tarot_card
 # import API manager module 
 
 app = Flask(__name__)
@@ -10,18 +14,17 @@ def homepage():
 
 @app.route('/mystic_fetcher')
 def mystic_results():
-    birthday = request.args 
-    print(birthday)
+    zodiac = request.args 
+    print(zodiac)
 
     # what was the user's birthday?
-    year = birthday.get('year')
-    month = birthday.get('month')
+    zodiac = zodiac.get('zodiac')
     
     # talk to apis
     # for example, api returns your spirit animal is zebra your horoscope is you will have a nice day 
     # this is example data, replace with data from the API 
-    animal = 'zebra'
-    horoscope = 'you will have a nice day'
+    animal = animal_url.get()  
+    horoscope = tarot_card.get()
     animal_url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Plains_Zebra_Equus_quagga.jpg/1024px-Plains_Zebra_Equus_quagga.jpg'
 
     # create new web page with results 
