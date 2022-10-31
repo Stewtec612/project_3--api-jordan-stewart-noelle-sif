eef88c45415fda731403691fc938f3714c0dc563
Jordan bundy <jbundy611@gmail.com>
2022-10-30 20:59:15 -0500

    got rid of global variables and renamed functions

--- api_manager.py
+++ api_manager.py
@@ -5,28 +5,23 @@ import horoscope_api
 from fate_store import Fate
 from get_user_info import get_user_info
 
-zodiac_sign = get_user_info()[1]
-
-card_meaning = tarot_api.get_tarot_meaning()   
-card_name = tarot_api.get_tarot_name()   
-animal_name = animal_api.get_animal_name()
-animal_picture = animal_api.get_animal_picture()
-horoscope = horoscope_api.get_horoscope_details()
-time = horoscope_api.get_time_of_horoscope()
-
 api_data = {}
 
 
-def main():
+def api_dictionary():
 
-    api_data.update({"Cards Name": card_name,"Tarot Card Meaning": card_meaning, "Animals Name": animal_name,
-    "Animal's Picture": animal_picture, "Time": time, "Users Horoscope": horoscope})
+    api_data.update({"Cards Name": tarot_api.get_tarot_name(),"Tarot Card Meaning": tarot_api.get_tarot_meaning(), "Animals Name": animal_api.get_animal_name(),
+    "Animal's Picture": animal_api.get_animal_picture(), "Time":horoscope_api.get_time_of_horoscope(), "Users Horoscope": horoscope_api.get_horoscope_details()})
 
     return api_data
 
-def to_db():
+def create_fate_object():
 
-    animal = [animal_name, animal_picture]
-    tarot_card = [card_meaning, card_name]
-    return Fate(animal, tarot_card, horoscope, zodiac_sign)
+    animal = animal_api.get_animal()
+    tarot_card = tarot_api.get_tarot_card()
+    horoscope = horoscope_api.get_horoscope()
+    zodiac_sign = get_user_info()[1]
+
+    return Fate(zodiac_sign, tarot_card, animal, horoscope)
+    
     
