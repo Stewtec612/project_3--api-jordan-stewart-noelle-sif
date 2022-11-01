df700fa4cd6277b7e4a669fa6703e5085d2ce110
Stewart Christie <stewart.chrst99@gmail.com>
2022-11-01 16:39:29 -0500

    created error handing for animal_api

--- animal_api.py
+++ animal_api.py
@@ -3,32 +3,45 @@ import requests
 
 
 def get_animal():
-    # get an animal (at random) for the user, which includes the name & a picture
-    animal_url = "https://zoo-animal-api.herokuapp.com/animals/rand"
-    response = requests.get(animal_url)
-    animal_data = response.json()
+    try:
+        # get an animal (at random) for the user, which includes the name & a picture
+        animal_url = "https://zoo-animal-api.herokuapp.com/animals/rand"
+        response = requests.get(animal_url)
+        response.raise_for_status()
+        animal_data = response.json()
 
-    animal_picture = get_animal_picture(animal_data)
-    animal_name = get_animal_name(animal_data)
-    full_animal_info = [animal_name, animal_picture]
+        animal_picture = get_animal_picture(animal_data)
+        animal_name = get_animal_name(animal_data)
+        full_animal_info = [animal_name, animal_picture]
 
-    # for testing - currently just shows the link to the animal pic in the terminal
-    print(full_animal_info)
+        # for testing - currently just shows the link to the animal pic in the terminal
+        print(full_animal_info)
 
-    return full_animal_info
-
+        return full_animal_info,None
+    except Exception as ex:
+        print(ex)
+        return None,ex
 
 def get_animal_picture(animal_data):
     #grabs the animal picture link from the API
     animal_picture = animal_data['image_link']
-
-    return animal_picture
+    if animal_picture is None:
+        return None
+    else:
+        return animal_picture
 
 
 def get_animal_name(animal_data):
     #grabs the animals name from the API
     animal_name = animal_data['name']
+    if animal_name is None:
+        return None
     
-    return animal_name
+    else:
+        return animal_name
     
-get_animal()
+def main():
+    get_animal()
+
+if __name__ == '__main__':
+    main()
