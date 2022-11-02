3c5341da3ab6e876bd04022f464d73e15560f3f0
Jordan Bundy <jbundy611@gmail.com>
2022-11-01 22:38:44 -0500

    added tarot meaning and name to web page

--- app.py
+++ app.py
@@ -28,7 +28,8 @@ def mystic_results():
     animal_name = get_animal_name()
     horoscope = get_horoscope(user_zodiac)
     animal_image_url = get_animal_picture()
-    tarot_card_url = get_tarot_card()
+    tarot_card_name = get_tarot_card()[0]
+    tarot_card_meaning = get_tarot_card[1]
 
     # # TODO for testing
     # animal_name = 'cheetah'
@@ -37,7 +38,7 @@ def mystic_results():
     # tarot_card_url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/RWS_Tarot_00_Fool.jpg/255px-RWS_Tarot_00_Fool.jpg'
 
     # create new web page with results 
-    return render_template('results.html', animal_name=animal_name, horoscope=horoscope, animal_image_url=animal_image_url, tarot_card_url=tarot_card_url)
+    return render_template('results.html', animal_name=animal_name, horoscope=horoscope, animal_image_url=animal_image_url, tarot_card_name=tarot_card_name,tarot_card_meaning=tarot_card_meaning)
 
 
 @app.route('/save_fate', methods=['POST'])
